6e8f069cdc6c2914982f5930401b244fe9224ed7
lagusdk <llahaoooo@gmail.com>
2024-11-04 17:11:02 +0900

    오류 수정

--- nav-map/src/App.js
+++ nav-map/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
-import LoginPage from './LoginPage'; // LoginPage 컴포넌트 임포트
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />  {/* LoginPage 컴포넌트를 렌더링 */}
     </div>
   );
 }
