410f305ae632f1470aad43a7c8e02f90f26c3359
junsy0901 <155941121+junsy0901@users.noreply.github.com>
2024-11-12 13:58:01 +0900

    [Fix] About 페이지 Prettier 정렬 적용

--- nav-map/src/pages/AboutTeanet.jsx
+++ nav-map/src/pages/AboutTeanet.jsx
@@ -14,54 +14,102 @@ import 이은송 from "../imgs/이은송.png";
 import 장윤선 from "../imgs/장윤선.png";
 
 const teamMembers = [
-    { name: "라현아", role: "학습모임 리더, FE", major: "Visual Technology 전공", github: "lagusdk", photo: 라현아 },
-    { name: "이하랑", role: "Back-end 파트장", major: "Data Science 전공", github: "XII28", photo: 이하랑 },
-    { name: "김다솔", role: "BE", major: "Data Science 전공", github: "dasolkim7", photo: 김다솔 },
-    { name: "최은비", role: "BE", major: "Data Science 전공", github: "dmsqlder", photo: 최은비 },
-    { name: "전서연", role: "Front-end 파트장", major: "Visual Technology 전공", github: "junsy0901", photo: 전서연 },
-    { name: "유아름", role: "FE", major: "Visual Technology 전공", github: "yooaknow", photo: 유아름 },
-    { name: "이은송", role: "FE", major: "Visual Technology 전공", github: "shoungli415", photo: 이은송 },
-    { name: "장윤선", role: "FE", major: "Visual Technology 전공", github: "yunseonny", photo: 장윤선 },
-  ];
+  {
+    name: "라현아",
+    role: "학습모임 리더, FE",
+    major: "Visual Technology 전공",
+    github: "lagusdk",
+    photo: 라현아,
+  },
+  {
+    name: "이하랑",
+    role: "Back-end 파트장",
+    major: "Data Science 전공",
+    github: "XII28",
+    photo: 이하랑,
+  },
+  {
+    name: "김다솔",
+    role: "BE",
+    major: "Data Science 전공",
+    github: "dasolkim7",
+    photo: 김다솔,
+  },
+  {
+    name: "최은비",
+    role: "BE",
+    major: "Data Science 전공",
+    github: "dmsqlder",
+    photo: 최은비,
+  },
+  {
+    name: "전서연",
+    role: "Front-end 파트장",
+    major: "Visual Technology 전공",
+    github: "junsy0901",
+    photo: 전서연,
+  },
+  {
+    name: "유아름",
+    role: "FE",
+    major: "Visual Technology 전공",
+    github: "yooaknow",
+    photo: 유아름,
+  },
+  {
+    name: "이은송",
+    role: "FE",
+    major: "Visual Technology 전공",
+    github: "shoungli415",
+    photo: 이은송,
+  },
+  {
+    name: "장윤선",
+    role: "FE",
+    major: "Visual Technology 전공",
+    github: "yunseonny",
+    photo: 장윤선,
+  },
+];
 
-  const AboutTeanet = () => {
-    return (
-      <div className="team-container">
-        <h2>TeaNet은...</h2>
-        <p className="team-description">
-                광운대학교 정보융합학부의 2학년 개발자들이 모여, <br />
-                소통과 협업을 통해 함께 성장하고 발전하는 팀입니다.
-        </p>
-        <div className="team-grid">
-          {teamMembers.map((member, index) => (
-            <div className="team-member" key={index}>
-              <div
-                className="member-photo"
-                style={{
-                  backgroundImage: `url(${member.photo})`,
-                }}
-                alt={`${member.name} 사진`}
-              ></div>
-              <h3>{member.name}</h3>
-              <p className="member-role">{member.role ? member.role : "팀원"}</p>
-              <p className="member-major">{member.major}</p>
-              {member.github && (
-                <a
-                  href={`https://github.com/${member.github}`}
-                  className="member-github"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  style={{ textDecoration: "none" }}
-                >
-                  <img src={githubIcon} alt="GitHub 아이콘" />
-                  <span className="github-id">{member.github}</span>
-                </a>
-              )}
-            </div>
-          ))}
-        </div>
+const AboutTeanet = () => {
+  return (
+    <div className="team-container">
+      <h2>TeaNet은...</h2>
+      <p className="team-description">
+        광운대학교 정보융합학부의 2학년 개발자들이 모여, <br />
+        소통과 협업을 통해 함께 성장하고 발전하는 팀입니다.
+      </p>
+      <div className="team-grid">
+        {teamMembers.map((member, index) => (
+          <div className="team-member" key={index}>
+            <div
+              className="member-photo"
+              style={{
+                backgroundImage: `url(${member.photo})`,
+              }}
+              alt={`${member.name} 사진`}
+            ></div>
+            <h3>{member.name}</h3>
+            <p className="member-role">{member.role ? member.role : "팀원"}</p>
+            <p className="member-major">{member.major}</p>
+            {member.github && (
+              <a
+                href={`https://github.com/${member.github}`}
+                className="member-github"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <img src={githubIcon} alt="GitHub 아이콘" />
+                <span className="github-id">{member.github}</span>
+              </a>
+            )}
+          </div>
+        ))}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default AboutTeanet;
