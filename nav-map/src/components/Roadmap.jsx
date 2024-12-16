e80dba85e7fd3b169eab387d6157c718554f1f66
junsy0901 <junsy0901@gmail.com>
2024-12-17 00:54:32 +0900

    [feat] 더미데이터 추가

--- nav-map/src/components/Roadmap.jsx
+++ nav-map/src/components/Roadmap.jsx
@@ -3,42 +3,10 @@ import ReactFlow from "react-flow-renderer";
 import "react-flow-renderer/dist/style.css";
 import "../css/Roadmap.css";
 
-const initialNodes = [
-  {
-    id: "1",
-    position: { x: 0, y: 0 },
-    data: { label: "프로그래밍기초" },
-    type: "default",
-  },
-  {
-    id: "2",
-    position: { x: 0, y: 100 },
-    data: { label: "이산수학" },
-    type: "default",
-  },
-  {
-    id: "3",
-    position: { x: 200, y: 100 },
-    data: { label: "인공지능과컴퓨팅사고" },
-    type: "default",
-  },
-  {
-    id: "4",
-    position: { x: 0, y: 200 },
-    data: { label: "AI수학" },
-    type: "default",
-  },
-];
-
-const initialEdges = [
-  { id: "e1-2", source: "1", target: "3", animated: true },
-  { id: "e1-2", source: "2", target: "4", animated: true },
-];
-
-const Roadmap = () => {
+const Roadmap = ({ nodes, edges }) => {
   return (
     <div className="roadmap-container">
-      <ReactFlow nodes={initialNodes} edges={initialEdges} fitView />
+      <ReactFlow nodes={nodes} edges={edges} fitView />
     </div>
   );
 };
--- nav-map/src/pages/GenerateRoadmap.jsx
+++ nav-map/src/pages/GenerateRoadmap.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Roadmap from "../components/Roadmap";
 import GenerateHeader from "../components/GenerateHeader";
 import "../css/Roadmap.css";
 
 const GenerateRoadmap = () => {
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
+
+  useEffect(() => {
+    fetch("/data3.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setNodes(data.initialNodes);
+        setEdges(data.initialEdges);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="generate-roadmap-container">
-      <GenerateHeader />
-      <Roadmap />
+      <GenerateHeader isCareer={3} />
+      <Roadmap nodes={nodes} edges={edges} />
     </div>
   );
 };
